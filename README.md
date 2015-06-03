59be46ff37811da1bcc8b3fb51c10b96730d2ca3
Benoît Simard (@logisima) <contact@bsimard.com>
2015-06-04 00:46:59 +0200

    Produce doc into  README file

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# neo4j-fwd
-Neo4j foreign data wrapper for Postgresql
+"""
+Purpose
+-------
+Neo4j-fw is a foreign data wrapper for Postgresql. It can be used to access data stored into a Neo4j database from Postgresql
+
+Dependencies
+------------
+You will need the `py2neo`_ library.
+
+Connection options
+~~~~~~~~~~~~~~~~~~
+Connection options are
+``server``
+  The remote host of Neo4j (default is localhost)
+``port``
+  The remote port of Neo4j (default is 7474)
+``user``
+  The remote user for Neo4j
+``password``
+  The remote password for Neo4j
+``cypher``
+  The cypher query to construct teh virtual table
+
+When defining the table, the local column names will be used to retrieve the
+remote column data.
+
+Moreover, the local column types will be used to interpret the results in the remote table. 
+
+What does it do to reduce the amount of fetched data ?
+------------------------------------------------------
+- `quals` are pushed to the remote database whenever possible. This include
+  simple operators :
+    - equality, inequality (=, <>, >, <, <=, >=)
+    - like, ilike and their negations
+    - IN clauses with scalars, = ANY (array)
+    - NOT IN clauses, != ALL (array)
+- the set of needed columns is pushed to the remote base, and only those columns will be fetched.
+
+Usage example
+-------------
+
+.. code-block:: sql
+  CREATE SERVER multicorn_neo4j FOREIGN DATA WRAPPER multicorn
+  OPTIONS (
+      wrapper  'neo4jfdw.Neo4jForeignDataWrapper',
+      server   'localhost',
+      port     '7474',
+      user     'neo4j',
+      password 'admin'
+  );
+  CREATE FOREIGN TABLE neo4j_movie (
+    movie varchar
+  ) SERVER multicorn_neo4j OPTIONS (
+    cypher 'MATCH (n:Movie) RETURN n.title as movie'
+  );
--- neo4jfdw.py
+++ neo4jfdw.py
@@ -1,51 +1,126 @@
 from multicorn import ForeignDataWrapper
 from multicorn.utils import log_to_postgres, ERROR, WARNING, DEBUG
-from py2neo import Graph
+from py2neo import neo4j
+import re
+
 
 class Neo4jForeignDataWrapper(ForeignDataWrapper):
     """
     Neo4j FWD for Postgresql
     """
-    
+
     def __init__(self, options, columns):
-      super(Neo4jForeignDataWrapper, self).__init__(options, columns)
-      
-      if 'server' not in options:
-            log_to_postgres('The server parameter is required and the default is http://localhost:7474', WARNING)
-      self.server = options.get("server", "http://localhost:7474")
-      
-      if 'user' not in options:
-          log_to_postgres('The user parameter is required for Neo4j > 2.2', WARNING)
-      self.user = options.get("user", None)
-      
-      if 'password' not in options:
-          log_to_postgres('The password parameter is required for Neo4j > 2.2', WARNING)
-      self.password = options.get("password", None)
-    
-      if 'cypher' not in options:
-          log_to_postgres('The cypher parameter is required', ERROR)
-      self.cypher = options.get("cypher", None)
-      
-      self.columns = columns
-    
-    
-    def execute(self, quals, columns):
-        if self.cypher:
-            statement = self.cypher
-        else:
-            statement = "MATCH (n) RETURN n LIMIT 100" 
-            
-        log_to_postgres('Neo4j query: ' + unicode(statement), DEBUG)
-        
-        # Manage py2neo authentification
+        # Calling super constructor
+        super(Neo4jForeignDataWrapper, self).__init__(options, columns)
+
+        # Managed server option
+        if 'server' not in options:
+            log_to_postgres('The server parameter is required and the default is "localhost"', WARNING)
+        self.server = options.get("server", "localhost:7474")
+
+        if 'port' not in options:
+            log_to_postgres('The port parameter is required and the default is "7474"', WARNING)
+        self.port = options.get("port", "7474")
+
+        if 'user' not in options:
+            log_to_postgres('The user parameter is required for Neo4j > 2.2', WARNING)
+        self.user = options.get("user", None)
+
+        if 'password' not in options:
+            log_to_postgres('The password parameter is required for Neo4j > 2.2', WARNING)
+        self.password = options.get("password", None)
+
+        if 'cypher' not in options:
+            log_to_postgres('The cypher parameter is required', ERROR)
+        self.cypher = options.get("cypher", "MATCH (n) RETURN n LIMIT 100")
+
+        # Setting table columns
+        self.columns = columns
+
+        # Create a py2neo graph instance
         if self.user and self.password:
-            authenticate(self.server, self.user, self.password)
-	
-	# Make the query
-        graph = Graph(self.server + "/db/data/")
-        for record in graph.cypher.stream(statement):
-	    line = {}
-	    for column_name in self.columns:
-                line[column_name] = record[column_name]
+            neo4j.authenticate(self.server + ":" + self.port, self.user, self.password)
+        self.graph = neo4j.Graph("http://" + self.server + ":" + self.port + "/db/data/")
+
+
+    def execute(self, quals, columns):
+
+        log_to_postgres('Query Columns:  %s' % columns, DEBUG)
+        log_to_postgres('Query Filters:  %s' % quals, DEBUG)
+
+        statement = self.make_cypher(quals, columns)
+        log_to_postgres('Neo4j query: ' + unicode(statement), DEBUG)
+
+        # Execute & retrieve neo4j data
+        for record in self.graph.cypher.stream(statement):
+            line = {}
+            for column_name in self.columns:
+                if hasattr(record, column_name):
+                    line[column_name] = record[column_name]
             yield line
+
+    def make_cypher(self, quals, columns):
+        """
+        Override cypher query to add search criteria
+        """
+        cypher = self.cypher
+        where_clause = 'AND '.join(self.extract_conditions(quals))
+
+        pattern = re.compile('(.*)RETURN(.*)', re.IGNORECASE)
+        match = pattern.match(self.cypher)
+        if len(where_clause) > 0 and match:
+            cypher = match.group(1) + "WITH" + match.group(2) + " WHERE " + where_clause + " RETURN " + ', '.join(columns)
+
+        return cypher
+
+
+    def extract_conditions(self, quals):
+        """
+        Build a neo4j search criteria string from a list of quals
+        """
+        conditions = []
+        for qual in quals:
+            conditions.append(self.make_condition(qual.field_name, qual.operator, qual.value))
+
+        conditions = [x for x in conditions if x not in (None, '()')]
+        return conditions
+
+
+    def make_condition(self, key, operator, value):
+        """
+        Build a neo4j condition from a qual
+        """
+        if operator not in ('~~', '!~~', '~~*', '!~~*', '=', '<>', '>=', '<='):
+            return ''
+
+        condition = ''
+
+        # Managed LIKE & ILIKE
+        if operator in ('~~', '!~~', '~~*', '!~~*'):
+            # Convert to cypher regex
+            regex = value.replace('%', '.*')
+
+            # For the negation, we prefix with NOT
+            if operator in ('!~~', '!~~*'):
+                condition += 'NOT '
+
+            # Adding the variable name
+            condition += key + "=~'"
+
+            # If it's a ILIKE, we prefix regex with (?i)
+            if operator in ('~~*', '!~~*'):
+                condition += '(?i)'
+
+            # We add the regex
+            condition += regex + "'"
+
+        else:
+            condition = key + operator + value
+
+        return condition
+
+# def insert(self, new_values):
+# def update(self, old_values, new_values):
+#def delete(self, old_values):
+      
             
