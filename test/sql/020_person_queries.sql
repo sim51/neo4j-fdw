baa3deef6bd25f887f628ab57353d509a2d5ba14
Benoit Simard <contact@bsimard.com>
2018-12-09 23:36:29 +0100

    Adding regress test on person

---
+++ test/sql/020_person_queries.sql
@@ -0,0 +1,5 @@
+SELECT * FROM person LIMIT 10;
+SELECT * FROM person p WHERE p.born > 1980 ORDER BY p.born DESC LIMIT 10;
+SELECT * FROM person p WHERE p.name = 'Keanu Reeves';
+SELECT * FROM person p WHERE p.name LIKE '%Keanu%';
+SELECT * FROM person p WHERE p.name ILIKE '%keanu%';
