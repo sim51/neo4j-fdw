88be69a2cbf4dde1f1a34ae7318ba7124b585966
Benoit Simard <contact@bsimard.com>
2019-04-17 17:26:53 +0200

    Better release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,6 +9,10 @@
 #  - git
 #  - github-release-cli (https://www.npmjs.com/package/github-release-cli)
 ###############################################################################
+version=$(grep default_version ./neo4j-fdw.control | sed -e "s/default_version[[:space:]]*=[[:space:]]*'\([^']*\)'/\1/" | sed "s/-dev//")
+echo "Release version is $version"
+next_version=""
+read -e -p "What is the next version : " -i "$next_version" next_version
 
 git update-index -q --refresh
 CHANGED=$(git diff-index --name-only HEAD --)
@@ -21,36 +25,39 @@ fi
 echo "Pushing"
 git push
 
+echo "Pushing"
+git push
+
 echo "~ Generate the project archive"
-git archive --format zip --prefix=neo4j-fdw_v$1/ --output ./neo4j-fdw_v$1.zip HEAD
-unzip ./neo4j-fdw_v$1.zip
-rm ./neo4j-fdw_v$1.zip
-sed -i -e "s/@@VERSION@@/$1/g" ./neo4j-fdw_v$1/META.json ./neo4j-fdw_v$1/setup.py ./neo4j-fdw_v$1/neo4jPg/__init__.py
-zip -r ./neo4j-fdw_v$1.zip ./neo4j-fdw_v$1/
-rm -rf neo4j-fdw_v$1/
+git archive --format zip --prefix=neo4j-fdw_v$version/ --output ./neo4j-fdw_v$version.zip HEAD
+unzip ./neo4j-fdw_v$version.zip
+rm ./neo4j-fdw_v$version.zip
+sed -i -e "s/__VERSION__/$version/g" ./neo4j-fdw_v$version/META.json ./neo4j-fdw_v$version/neo4j-fdw.control ./neo4j-fdw_v$version/setup.py ./neo4j-fdw_v$version/neo4jPg/__init__.py
+zip -r ./neo4j-fdw_v$version.zip ./neo4j-fdw_v$version/
+rm -rf neo4j-fdw_v$version/
 
 echo "~ Generate release notes"
 
 tmpfile=$(mktemp /tmp/release-neo4j-fdw.XXXXXX)
-echo "# Neo4j FDW Release v$1" > "$tmpfile"
+echo "# Neo4j FDW Release v$version" > "$tmpfile"
 
 echo "Retrieving milestone issues"
-status=$(curl -w "%{http_code}" -s -o /dev/null -I -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/sim51/neo4j-fdw/issues?milestone=v$1")
+status=$(curl -w "%{http_code}" -s -o /dev/null -I -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/sim51/neo4j-fdw/issues?milestone=v$version")
 if [[ status == '200' ]]; then
 
   echo "" >> "$tmpfile"
   echo "## Fixes" >> "$tmpfile"
   echo "" >> "$tmpfile"
-  curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/sim51/neo4j-fdw/issues?milestone=v$1&state=closed&sort=updated&labels=bug" | jq -r '.[] | [(.number), .title] | @tsv' | sed s/^/#/g >> "$tmpfile"
+  curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/sim51/neo4j-fdw/issues?milestone=v$version&state=closed&sort=updated&labels=bug" | jq -r '.[] | [(.number), .title] | @tsv' | sed s/^/#/g >> "$tmpfile"
 
   echo "" >> "$tmpfile"
   echo "## Improvements" >> "$tmpfile"
   echo "" >> "$tmpfile"
-  curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/sim51/neo4j-fdw/issues?milestone=v$1&state=closed&sort=updated&labels=enhancement" | jq -r '.[] | [(.number), .title] | @tsv' | sed s/^/#/g >> "$tmpfile"
+  curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/sim51/neo4j-fdw/issues?milestone=v$version&state=closed&sort=updated&labels=enhancement" | jq -r '.[] | [(.number), .title] | @tsv' | sed s/^/#/g >> "$tmpfile"
 fi
 
 # retrieve last tag if not set
-latest=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/sim51/neo4j-fdw/tags" | jq -r '.[0].name')
+latest=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/sim51/neo4j-fdw/tags" | jq -r '.[0].commit.sha')
 echo "" >> "$tmpfile"
 echo "## Commits" >> "$tmpfile"
 echo "" >> "$tmpfile"
@@ -64,6 +71,11 @@ fi
 # Create the github release notes
 # based on https://www.npmjs.com/package/github-release-cli
 echo "Running github release github-release"
-github-release upload  --owner=sim51  --repo=neo4j-fdw  --tag="v$1"  --name="v$1"  --body="$(cat $tmpfile)"  neo4j-fdw_v$1.zip
+github-release upload  --owner=sim51  --repo=neo4j-fdw  --tag="v$version"  --name="v$1"  --body="$(cat $tmpfile)"  neo4j-fdw_v$version.zip
+
+echo "Go to next version for dev"
+cat ./neo4j-fdw.control | sed -e "s/default_version[[:space:]]*=[[:space:]]*'\([^']*\)'/default_version = '${next_version}-dev'/" > ./neo4j-fdw.control.new
+rm ./neo4j-fdw.control
+mv ./neo4j-fdw.control.new  ./neo4j-fdw.control
 
 echo "Don't forget to upload the zip file at https://manager.pgxn.org/upload"
