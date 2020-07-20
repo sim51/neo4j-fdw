6611dec4138234b4fbae74d33562e5372fba0054
James Holtom <jholtom@ciena.com>
2020-07-20 12:06:49 +0100

    Allow main-driver to receive updates, without getting clobbered by the v4.0 driver line.

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
       author_email     = neo4jPg.__email__,
       url              = neo4jPg.__url__,
       packages         = find_packages(),
-      install_requires = [ 'neo4j-driver==1.7.6','neobolt==1.7.16', 'python-dateutil' ]
+      install_requires = [ 'neo4j-driver>=1.7,<2.0','neobolt==1.7.16', 'python-dateutil' ]
 )
