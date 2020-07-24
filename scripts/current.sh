6f2e2e7ab9eb6f28cd7e2390e5268e409a6e3eae
James Holtom <jholtom@ciena.com>
2020-07-24 12:34:47 +0100

    Adjust test-case positioning scripts to silence file-not-present errors

--- scripts/current.sh
+++ scripts/current.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm /source/test/sql/000_create_extensions_wrappers_legacy.sql
-rm /source/test/expected/000_create_extensions_wrappers_legacy.out
+CSQL='/source/test/sql/000_create_extensions_wrappers_legacy.sql'; [[ -f $CSQL ]] && rm $CSQL
+COUT='/source/test/expected/000_create_extensions_wrappers_legacy.out'; [[ -f $COUT ]] && rm $COUT
 cp /source/test/sql/000_create_extensions_wrappers.current /source/test/sql/000_create_extensions_wrappers_current.sql
 cp /source/test/expected/000_create_extensions_wrappers.current /source/test/expected/000_create_extensions_wrappers_current.out
--- scripts/legacy.sh
+++ scripts/legacy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm /source/test/sql/000_create_extensions_wrappers_current.sql
-rm /source/test/expected/000_create_extensions_wrappers_current.out
+CSQL='/source/test/sql/000_create_extensions_wrappers_current.sql'; [[ -f $CSQL ]] && rm $CSQL
+COUT='/source/test/expected/000_create_extensions_wrappers_current.sql'; [[ -f $COUT ]] && rm $COUT
 cp /source/test/sql/000_create_extensions_wrappers.legacy /source/test/sql/000_create_extensions_wrappers_legacy.sql
 cp /source/test/expected/000_create_extensions_wrappers.legacy /source/test/expected/000_create_extensions_wrappers_legacy.out
